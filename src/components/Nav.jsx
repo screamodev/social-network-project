26205105a53fe4d06215816fd9d8f871f4cef708
screamoviolence <screamoviolence89@gmail.com>
2021-08-14 21:04:49 +0300

    add_components

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav className="nav"> 
+            <ul>
+              <li><a>Profile</a></li>
+              <li><a>Messages</a></li>
+              <li><a>News</a></li>
+              <li><a>Music</a></li>
+              <li><a>Settings</a></li>
+            </ul>
+          </nav>)
+}
+
+export default Nav;
