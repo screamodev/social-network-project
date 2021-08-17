9076863f8da74ff669da3409d579ba4d413321cb
screamoviolence <screamoviolence89@gmail.com>
2021-08-17 13:41:59 +0300

    links, pages, routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import './App.css';
-import Profile from './components/Profile';
-import Header from './components/Header';
-import Nav from './components/Nav';
+import Profile from './components/Profile/Profile';
+import Header from './components/Header/Header';
+import Navbar from './components/NavBar/Navbar';
+import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+
+
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Nav />
-      <Profile />
+    <BrowserRouter>
+      <div className='app-wrapper'>
 
-
-    </div>)
-
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Route path="/profile" component={Profile} />
+          <Route path="/dialogs" component={Dialogs} />
+          <Route path="/news" component={News} />
+          <Route path="/music" component={Music} />
+          <Route path="/settings" component={Settings} />
+        </div>
+      </div>
+    </BrowserRouter>)
 }
 
 export default App;
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = () => {
-    return (
-        <header className="header">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/240px-Instagram_logo_2016.svg.png" alt=''></img>
-        </header>)
-}
-export default Header;
