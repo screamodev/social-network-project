e923fc63f88b7a74596e075193d38f963937665f
screamoviolence <screamoviolence89@gmail.com>
2021-08-31 17:44:26 +0300

    added redux lib

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import './App.css';
-import Profile from './components/Profile/Profile';
-import Header from './components/Header/Header';
-import Navbar from './components/NavBar/Navbar';
-import Dialogs from './components/Dialogs/Dialogs';
-import News from './components/News/News';
-import Music from './components/Music/Music';
-import Settings from './components/Settings/Settings';
+import React from "react";
+import "./App.css";
+import Profile from "./components/Profile/Profile";
+import Header from "./components/Header/Header";
+import Navbar from "./components/NavBar/Navbar";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+import { Route } from "react-router-dom";
 
-
-import { BrowserRouter, Route } from "react-router-dom";
-
-const App = () => {
+const App = (props) => {
   return (
-    <BrowserRouter>
-      <div className='app-wrapper'>
-
-        <Header />
-        <Navbar />
-        <div className='app-wrapper-content'>
-          <Route path="/profile" component={Profile} />
-          <Route path="/dialogs" component={Dialogs} />
-          <Route path="/news" component={News} />
-          <Route path="/music" component={Music} />
-          <Route path="/settings" component={Settings} />
-        </div>
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      <div className="app-wrapper-content">
+        <Route path="/profile" render={() => <Profile store={props.store} />} />
+        <Route
+          path="/dialogs"
+          render={() => <DialogsContainer store={props.store} />}
+        />
+        <Route path="/news" component={News} />
+        <Route path="/music" component={Music} />
+        <Route path="/settings" component={Settings} />
       </div>
-    </BrowserRouter>)
-}
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/reduxStore.js";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App
+          state={state}
+          dispatch={store.dispatch.bind(store)}
+          store={store}
+        />
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
+
+rerenderEntireTree(store.getState());
+
+store.subscribe(() => {
+  let state = store.getState();
+  rerenderEntireTree(state);
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
